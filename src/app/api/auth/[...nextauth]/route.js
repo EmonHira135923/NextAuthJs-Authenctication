5609dbe7e81a7c57309e9b13fe58e7b41ae58667
EmonHira135923 <emonhossainhira231@gmail.com>
2026-01-26 04:05:45 -0800

    setup authentication

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      // Sign in with {name} button
+      name: "Credentials",
+
+      //   form inputs
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        //    my own logic
+        return null;
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
